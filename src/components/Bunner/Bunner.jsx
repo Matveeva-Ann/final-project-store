0994e777d0b88ad034bf28a2812f4aadc10e3384
Svitlana Pedchenko <tulipromashka@gmail.com>
2023-12-26 20:13:06 +0200

    add fonts,bunner

---
+++ src/components/Bunner/Bunner.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import {Title } from "./Bunner.style.jsx"
+
+
+    function Bunner() {
+        return (
+            <div>
+        <Title>CROCUS HOME DECOR</Title>
+            </ div>
+        )
+    
+}
+export default Bunner
