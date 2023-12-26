b076c46b6b52c8fae65d9f741421569d686e3b78
Anna Matveeva <107624838+Matveeva-Ann@users.noreply.github.com>
2023-12-26 21:59:14 +0200

    add fonts,bunner

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import './fonts/fonts.css'
 import './App.css';
+import Bunner from './components/Bunner/Bunner';
 
 export default function App() {
   return(
-    <></>
+    <Bunner />
   )
 }
---
+++ src/components/Bunner/Bunner.style.jsx
@@ -0,0 +1,9 @@
+import styled from "@emotion/styled";
+
+export const Title=styled.h1`
+    margin:0 auto;
+    font-family: 'Cormorant Garamond', serif;
+    /* color: var(--accentColorText) ; */
+    color:red;
+    opacity: 0.8;
+`
