f3d11ccaa3ff20201b7bf5289468ac6f0b7a4e03
Anna Matveeva <107624838+Matveeva-Ann@users.noreply.github.com>
2024-01-01 11:59:40 +0200

    The animation of the snowflakes has been changed

--- src/components/AnimationSnowfall/Snowfall.js
+++ src/components/AnimationSnowfall/Snowfall.js
@@ -2,21 +2,29 @@ import React, { useEffect } from 'react';
 import './style.css';
 
 const Snowfall = () => {
-  useEffect(() => {
-    const container = document.querySelector('.snowflakes');
 
-    for (let i = 0; i < 50; i++) {
+  useEffect(() => {
+    const container = document.getElementById('snowflakes');
+    for (let i = 0; i < 25; i++) {
       const snowflake = document.createElement('div');
       snowflake.className = 'snowflake';
       snowflake.innerHTML = '❅';
       snowflake.style.left = `${Math.random() * 100}vw`;
-      snowflake.style.animationDuration = `${Math.random() * 10 + 9}s`;
+      snowflake.style.animationDuration = `${Math.random() * 20 + 10}s`;
+      container.appendChild(snowflake);
+    }
+    for (let i = 0; i < 25; i++) {
+      const snowflake = document.createElement('div');
+      snowflake.className = 'snowflake2';
+      snowflake.innerHTML = '❅';
+      snowflake.style.left = `${Math.random() * 100}vw`;
+      snowflake.style.animationDuration = `${Math.random() * 20 + 10}s`;
       container.appendChild(snowflake);
     }
   }, []);
 
   return (
-    <div className="snowflakes" aria-hidden="true"></div>
+    <div className='snowflakes' id="snowflakes" aria-hidden="true"></div>
   );
 };
 
